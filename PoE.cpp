df273987ce2b276f7b82bb907e08744973a45746
Максим Костюченко <maksmaksim.2003@mail.ru>
2021-10-20 21:17:00 +0300

    Set initial blank form.

---
+++ PoE.cpp
@@ -0,0 +1,12 @@
+#include "start-form.h"
+#include <Windows.h>
+
+using namespace HotelInfoSystem;
+
+[STAThreadAttribute]
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+    Application::Run(gcnew startform);
+    return 0;
+}
